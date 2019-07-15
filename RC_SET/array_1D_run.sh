2170efb5ef75cc38fa8b6801a51660a9d6934dfc
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-07-15 15:58:54 +0300

    fixing scripts

--- RC_SET/array_1D_run.sh
+++ RC_SET/array_1D_run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-arrayN = ( 2 10 100 )
-vmin = 0
-vmax = 6
-vstep = 0.1
-tstep = 100
-vg_avg = 0
-c_avg = 1
-cg_avg = 1
-r_avg = 1
-rg_avg = 1000
-repeats = 10
-n_avg = 0
-n_std = 0
-q_avg = 0
-q_std = 0
-dt = 0.01
-folder = one_dimension_array_first_run
+arrayN=( 2 10 100 )
+vmin=0
+vmax=6
+vstep=0.1
+tstep=100
+vg_avg=0
+c_avg=1
+cg_avg=1
+r_avg=1
+rg_avg=1000
+repeats=10
+n_avg=0
+n_std=0
+q_avg=0
+q_std=0
+dt=0.01
+folder="one_dimension_array_first_run"
 
 for N in "${arrayN[@]}"
 do
