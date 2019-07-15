7b351177ebd3d73ed248146334efcbafd783fb0d
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-07-15 16:29:51 +0300

    fixing script again

--- RC_SET/array_1D_run.sh
+++ RC_SET/array_1D_run.sh
@@ -21,33 +21,32 @@ f="one_dimension_array_first_run"
 for N in "${arrayN[@]}"
 do
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q 
-./run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" no_disorder_N_"$N" "$f"
+qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" no_disorder_N_"$N" "$f"
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 10  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "r_disorder_N_$N" "$folder"   
+qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 10  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "r_disorder_N_$N" "$f"   
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_c_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 10 "$cg_avg" 0 "$r_avg" 0 "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "c_disorder_N_$N" "$folder" 
+qsub -V -S /bin/bash -cwd -N one_dimension_array_c_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 10 "$cg_avg" 0 "$r_avg" 0 "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "c_disorder_N_$N" "$f" 
 
 sleep 3
 
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_rg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "rg_disorder_N_$N" "$folder"  
+qsub -V -S /bin/bash -cwd -N one_dimension_array_rg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "rg_disorder_N_$N" "$f"  
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_cg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 10 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "cg_disorder_N_$N" "$folder"   
+qsub -V -S /bin/bash -cwd -N one_dimension_array_cg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 10 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "cg_disorder_N_$N" "$f"   
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_vg_disprder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "vg_disorder_N_$N" "$folder" 
+qsub -V -S /bin/bash -cwd -N one_dimension_array_vg_disprder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "vg_disorder_N_$N" "$f" 
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_all_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 10 "$cg_avg" 10 "$r_avg" 10  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "all_disorder_N_$N" "$folder"  
+qsub -V -S /bin/bash -cwd -N one_dimension_array_all_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 10 "$cg_avg" 10 "$r_avg" 10  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "all_disorder_N_$N" "$f"  
 
 sleep 3
 
