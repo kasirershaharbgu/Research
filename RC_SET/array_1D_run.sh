ecacdf6c76fdb23dca57663e3fc69f53cf109498
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-07-17 10:47:16 +0300

    ordering and updating simulation script

--- RC_SET/array_1D_run.sh
+++ RC_SET/array_1D_run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-arrayN=( 2 10 100 )
+arrayN=100
 vmin=0
-vmax=6
-vstep=0.1
-tstep=100
+tstep=1000
 vg_avg=0
 c_avg=1
 cg_avg=1
@@ -16,38 +14,33 @@ n_std=0
 q_avg=0
 q_std=0
 dt=0.01
-f="one_dimension_array_first_run"
+vmax=100
+vsep=1
+f="1D_Array/one_dimension_big_array"
 
-for N in "${arrayN[@]}"
-do
-
-qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" no_disorder_N_"$N" "$f"
-
-sleep 3
-
-qsub -V -S /bin/bash -cwd -N one_dimension_array_r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "r_disorder_N_$N" "$f"   
+qsub -V -S /bin/bash -cwd -N no_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" no_disorder_N_"$N" "$f"
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_c_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 1 "$cg_avg" 0 "$r_avg" 0 "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "c_disorder_N_$N" "$f" 
+qsub -V -S /bin/bash -cwd -N r_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "r_disorder_N_$N" "$f"   
+
+sleep 3
+
+qsub -V -S /bin/bash -cwd -N c_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0.1 "$cg_avg" 0 "$r_avg" 0 "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "c_disorder_N_$N" "$f" 
 
 sleep 3
 
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_rg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "rg_disorder_N_$N" "$f"  
+qsub -V -S /bin/bash -cwd -N rg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "rg_disorder_N_$N" "$f"  
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_cg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 10 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "cg_disorder_N_$N" "$f"   
+qsub -V -S /bin/bash -cwd -N cg_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 10 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "cg_disorder_N_$N" "$f"   
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_vg_disprder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "vg_disorder_N_$N" "$f" 
+qsub -V -S /bin/bash -cwd -N vg_disprder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "vg_disorder_N_$N" "$f" 
 
 sleep 3
 
-qsub -V -S /bin/bash -cwd -N one_dimension_array_all_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 1 "$cg_avg" 1 "$r_avg" 1  "$rg_avg" 100 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "all_disorder_N_$N" "$f"  
-
-sleep 3
-
-done
+qsub -V -S /bin/bash -cwd -N all_disorder -q jdubi.q run_RC_SET.sh 1 "$N" "$vmin" "$vmax" "$vstep" "$tstep" "$vg_avg" 10 "$c_avg" 0 "$cg_avg" 1 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "$dt" "all_disorder_N_$N" "$f"  
