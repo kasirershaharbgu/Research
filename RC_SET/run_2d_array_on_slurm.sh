adfc9006c76ccb14d67ade23ed173d0b807c1517
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-09-23 19:14:21 -0400

    fixing

--- RC_SET/run_2d_array_on_slurm.sh
+++ RC_SET/run_2d_array_on_slurm.sh
@@ -14,25 +14,25 @@ q_std=0
 vstep=0.1
 f="2D_Array/first_run"
 
-sbatch run_RC_SET.slurm 1 1 "$vmin" 2 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_1_1" "$f" 1
+sbatch run_RC_SET_on_slurm.sh 1 1 "$vmin" 2 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_1_1" "$f" 1
 
 sleep 3
 
-sbatch run_RC_SET.slurm 1 1 "$vmin" 2 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_1_1" "$f" 1  
+sbatch run_RC_SET_on_slurm.sh 1 1 "$vmin" 2 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_1_1" "$f" 1  
 
 sleep 3
 
-sbatch run_RC_SET.slurm 1 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_1_10" "$f" 5   
+sbatch run_RC_SET_on_slurm.sh 1 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_1_10" "$f" 5   
 
 sleep 3
 
 
-sbatch run_RC_SET.slurm 1 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_1_10" "$f" 5  
+sbatch run_RC_SET_on_slurm.sh 1 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_1_10" "$f" 5  
 
 sleep 3
 
-sbatch run_RC_SET.slurm 10 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_10_10" "$f" 10    
+sbatch run_RC_SET_on_slurm.sh 10 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 0  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "no_disorder_10_10" "$f" 10    
 
 sleep 3
 
-sbatch run_RC_SET.slurm 10 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_10_10" "$f" 10  
+sbatch run_RC_SET_on_slurm.sh 10 10 "$vmin" 20 "$vstep" "$vg_avg" 0 "$c_avg" 0 "$cg_avg" 0 "$r_avg" 1  "$rg_avg" 0 "$repeats" "$n_avg" "$n_std" "$q_avg" "$q_std" "r_disorder_10_10" "$f" 10  
