31b52e2739c3f3fd0f488a952ac137ef61d5b10b
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-09-23 15:58:50 -0400

    1 dot dbg

--- RC_SET/dots_array.py
+++ RC_SET/dots_array.py
@@ -4,6 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from optparse import OptionParser
 import os
+import cProfile, pstats
 from ast import literal_eval
 
 def flattenToColumn(a):
@@ -35,21 +36,24 @@ class DotArray:
         """
         self.rows = rows
         self.columns = columns
-        self.VL = Vext/2
-        self.VR = -Vext/2
+        self.VL = Vext
+        self.VR = -0.25
         self.VG = VG
         self.Q = Q0
         self.n = n0
         self.CG = CG
         self.RG = RG
         self.Ch = Ch
-        self.Cv = np.vstack((np.zeros((1,columns)),Cv))
+        self.Cv = np.vstack((np.zeros((1,columns)),Cv)) if Cv.size else np.zeros((1,1))
         self.Rh = Rh
         self.Rv = Rv
-        self.totalChargePassed = 0
+        self.R = np.hstack((Rh.flatten(), Rh.flatten(), Rv.flatten(), Rv.flatten()))
+        self.totalChargePassedRight = 0
+        self.totalChargePassedLeft = 0
         self.createCapacitanceMatrix()
         self.setDiagonalizedJ()
         self.setConstWork()
+        self.setConstMatrix()
 
     def getRows(self):
         return self.rows
@@ -61,16 +65,17 @@ class DotArray:
         """
         Reset the counter for total charge passed
         """
-        self.totalChargePassed = 0
+        self.totalChargePassedRight = 0
+        self.totalChargePassedLeft = 0
         return True
 
     def changeVext(self, newVext):
-        self.VL = newVext/2
-        self.VR = -newVext/2
+        self.VL = newVext
+        self.setConstWork()
         return True
 
     def getCharge(self):
-        return self.totalChargePassed
+        return self.totalChargePassedRight, self.totalChargePassedLeft
 
     def createCapacitanceMatrix(self):
         """
@@ -88,41 +93,47 @@ class DotArray:
         return True
 
     def getNprime(self):
-        left_part = np.copy(self.Ch)
+        left_part = self.Ch[:,:-1]
         left_part[:,1:] = 0
-        right_part = np.copy(self.Ch)
+        right_part = self.Ch[:,1:]
         right_part[:,:-1] = 0
         return flattenToColumn(self.n + left_part*self.VL + right_part*self.VR)
 
     def getbVector(self):
+        """
+        For developement of Q by dQ/dt=JQ +b
+        """
         res = -self.invC.dot(self.getNprime()) + flattenToColumn(self.VG)
         return res / np.repeat(flattenToColumn(self.RG),res.shape[1],axis=1)
 
     def getJmatrix(self):
+        """
+        For developement of Q by dQ/dt=JQ +b
+        """
         res = self.invC + np.diagflat(1/self.CG)
-        return res / np.repeat(flattenToColumn(self.RG),res.shape[1],axis=1)
+        return -res / np.repeat(flattenToColumn(self.RG),res.shape[1],axis=1)
 
     def setDiagonalizedJ(self):
         self._JeigenValues, self._JeigenVectors = np.linalg.eig(self.getJmatrix())
-        self._JeigenVectorsInv = np.linalg.inv(self.JeigenVectors)
+        self._JeigenVectorsInv = np.linalg.inv(self._JeigenVectors)
         return True
 
     def developeQ(self, dt):
-        Q0 = self._JeigenVectorsInv.dot(self.Q.T)
-        b = self.getbVector()
+        Q0 = self._JeigenVectorsInv.dot(flattenToColumn(self.Q))
+        b = self._JeigenVectorsInv.dot(self.getbVector())
         exponent = np.exp(self._JeigenValues*dt)
         self.Q = self._JeigenVectors.dot(Q0*exponent +(
-             b/self._JeigenValues)*(exponent - 1)).T
+             b/self._JeigenValues)*(exponent - 1)).reshape((self.rows, self.columns))
         return True
 
     def setConstWork(self):
         invCDiagMat = np.diag(self.invC).reshape((self.rows,self.columns))
         lowerCDiag = np.pad(np.diag(self.invC,k=-1),((0,1),)).reshape((self.rows,self.columns))
         lowerCDiag[:,:-1] = 0
-        lowerCDiag  = np.pad(lowerCDiag,((1,0),(0,0)))
+        lowerCDiag  = np.pad(lowerCDiag,((0,0),(1,0)))
         upperCDiag = np.pad(np.diag(self.invC,k=1),((0,1),)).reshape((self.rows,self.columns))
         upperCDiag[:,:-1] = 0
-        upperCDiag  = np.pad(upperCDiag,((1,0),(0,0)))
+        upperCDiag  = np.pad(upperCDiag,((0,0),(1,0)))
         commonHorz = np.pad(invCDiagMat,((0,0),(1,0))) + np.pad(invCDiagMat,((0,0),(0,1)))\
                         - lowerCDiag - upperCDiag
 
@@ -133,47 +144,54 @@ class DotArray:
         additionalLeft = np.zeros((self.rows,self.columns+1))
         additionalLeft[:,0] = self.VL
         additionalLeft[:,-1] = -self.VR
-        self.leftConstWork = (0.5*commonHorz + additionalLeft).flatten()
+        leftConstWork = (0.5*commonHorz + additionalLeft).flatten()
 
         additionalRight = -additionalLeft
-        self.rightConstWork = (0.5*commonHorz + additionalRight).flatten()
+        rightConstWork = (0.5*commonHorz + additionalRight).flatten()
 
-        self.upConstWork = (0.5*commonVert).flatten()
-        self.downConstWork = np.copy(self.upConstWork)
+        vertConstWork = (0.5*commonVert).flatten()
+
+        self.constWork = np.hstack((rightConstWork, leftConstWork, vertConstWork, vertConstWork))
         return True
 
-    def getWork(self):
-        q = self.getNprime() + flattenToColumn(self.QG)
-        firstHorzMat = np.zeros(((self.columns+1)*self.rows,self.columns*self.rows))
+    def setConstMatrix(self):
+        firstHorzMat = np.zeros(((self.columns + 1) * self.rows, self.columns * self.rows))
         secondHorzMat = np.zeros(firstHorzMat.shape)
-        firstLocations = np.ones(((self.columns+1)*self.rows,))
+        firstLocations = np.ones(((self.columns + 1) * self.rows,))
         secondLocations = np.ones(firstLocations.shape)
-        firstLocations[self.columns:self.columns+1:] = 0
-        secondLocations[0:self.columns+1:] = 0
-        firstHorzMat[np.astype(firstLocations,np.bool),:] = self.invC
-        secondHorzMat[np.astype(secondLocations,np.bool),:] = self.invC
-        variableRightWork = ((firstHorzMat - secondHorzMat).dot(q)).flatten()
-        variableLeftWork = -variableRightWork
+        firstLocations[self.columns::self.columns + 1] = 0
+        secondLocations[0::self.columns + 1] = 0
+        firstHorzMat[firstLocations.astype(np.bool), :] = self.invC
+        secondHorzMat[secondLocations.astype(np.bool), :] = self.invC
+        self.horizontalMatrix = firstHorzMat - secondHorzMat
+        firstVertMat = self.invC[:-self.columns, :]
+        secondVertMat = self.invC[self.columns:, :]
+        self.verticalMatrix = firstVertMat - secondVertMat
+        return True
 
-        firstVertMat = self.invC[:-self.columns,:]
-        secondVertMat = self.invC[self.columns:,:]
-        variableUpWork = ((firstVertMat - secondVertMat).dot(q)).flatten()
-        variableDownWork = -variableUpWork
 
-        return variableRightWork + self.rightConstWork, variableLeftWork + self.leftConstWork,\
-               variableDownWork + self.downConstWork, variableUpWork + self.upConstWork
+
+    def getWork(self):
+        q = self.getNprime() + flattenToColumn(self.Q)
+        variableRightWork = (self.horizontalMatrix.dot(q)).flatten()
+        variableUpWork = (self.verticalMatrix.dot(q)).flatten()
+        variableWork = np.hstack((variableRightWork, -variableRightWork, -variableUpWork, variableUpWork))
+
+        return variableWork + self.constWork
 
     def getRates(self):
-        rightWork, leftWork, downWork, upWork = self.getWork()
-        horzResistance = self.Rh.flatten()
-        vertResistance = self.Rv.flatten()
-        rightWork[rightWork > 0] = 0
-        leftWork[leftWork > 0] = 0
-        downWork[downWork > 0] = 0
-        upWork[upWork > 0] = 0
-        return -rightWork/horzResistance, -leftWork/horzResistance,\
-               -downWork/vertResistance,-upWork/vertResistance
+        work = self.getWork()
+        work[work > 0] = 0
+        return -work / self.R
 
+    def getTimeInterval(self):
+        rates = self.getRates()
+        max_rate = np.max(rates)
+        if max_rate > 0:
+            dt = np.log(1.05) / max_rate
+            return dt
+        else:
+            return None
 
     def getprobabilities(self, dt):
         rates = self.getRates()
@@ -182,14 +200,18 @@ class DotArray:
         return prob
 
     def tunnel(self, fromDot, toDot):
-        if self.columns > fromDot[1] >= 0:
-            self.n[fromDot] -=1
-        if self.columns > toDot[1] >= 0:
-            self.n[toDot] += 1
+        if fromDot[1] == self.columns:
+            self.totalChargePassedRight -= 1
+        elif fromDot[1] == -1:
+            self.totalChargePassedLeft -= 1
+        else:
+            self.n[fromDot] -= 1
         if toDot[1] == self.columns:
-            self.totalChargePassed += 1
-        elif fromDot[1] == self.columns:
-            self.totalChargePassed -= 1
+            self.totalChargePassedRight += 1
+        elif toDot[1] == -1:
+            self.totalChargePassedLeft += 1
+        else:
+            self.n[toDot] += 1
         return True
 
     def printState(self):
@@ -200,23 +222,23 @@ class DotArray:
                     self.Q[i,j]))
 
     def executeStep(self, ind):
-            horzSize = (self.rows*(self.columns+1))**2
-            vertSize = ((self.rows - 1)*self.columns)**2
-            if ind <  horzSize: # tunnel right:
-                fromDot = (ind//(self.columns+1), ind%(self.columns+1)-1)
-                toDot = fromDot + (0,1)
+            horzSize = self.rows*(self.columns+1)
+            vertSize = (self.rows - 1)*self.columns
+            if ind < horzSize: # tunnel right:
+                fromDot = (ind//(self.columns+1), (ind%(self.columns+1))-1)
+                toDot = (fromDot[0], fromDot[1] + 1)
             elif ind < horzSize*2:
                 ind -= horzSize
                 fromDot = (ind//(self.columns+1), ind%(self.columns+1))
-                toDot = fromDot + (0,-1)
-            elif ind < horzSize*2 + vertSize:
+                toDot = (fromDot[0], fromDot[1] - 1)
+            elif ind < horzSize*2 + vertSize: #tunnel down
                 ind -= horzSize*2
                 fromDot = (ind//self.columns, ind%self.columns)
-                toDot = fromDot + (1,0)
+                toDot = (fromDot[0] + 1, fromDot[1])
             else:
                 ind -= (horzSize*2 + vertSize)
                 fromDot = (ind//self.columns+1, ind%self.columns)
-                toDot = fromDot + (-1,0)
+                toDot = (fromDot[0] - 1, fromDot[1])
             self.tunnel(fromDot, toDot)
 
 MINPROBLOG = np.log(1/0.7)
@@ -226,12 +248,10 @@ class Simulator:
     :param rows: number of rows in the array
     :param columns: number of columns in the array
     """
-    def __init__(self, rows, columns, Vext0, VG, dt,
+    def __init__(self, rows, columns, Vext0, VG,
                  Q0, n0, CG, RG, Ch, Cv, Rh, Rv):
         self.dotArray = DotArray(rows, columns, Vext0, VG, Q0, n0, CG, RG,
                                  Ch, Cv, Rh, Rv)
-        self.t = 0
-        self.dt = dt
         self.Vext = Vext0
         # for debug
     #     self.randomGen = self.getRandom()
@@ -245,18 +265,22 @@ class Simulator:
     #             idx = 0
     #         yield numbers[idx]
 
-    def executeStep(self, printState=False):
+    def executeStep(self, timeLeft, printState=False):
         r = np.random.ranf()
         # for debug
         # r = next(self.randomGen)
         probNotOk = True
+        dt = self.dotArray.getTimeInterval()
+        if dt is None:
+            dt = timeLeft
         while (probNotOk):
-            self.dotArray.developeQ(self.dt)
-            prob = self.dotArray.getprobabilities(self.dt)
+            self.dotArray.developeQ(dt)
+            prob = self.dotArray.getprobabilities(dt)
             cumProb = np.cumsum(prob)
             if cumProb[-1] > 1:
                 print("Warning: total prob > 1 needed smaller dt, reducing dt")
-                self.dt /= 10
+                self.dotArray.developeQ(-dt)
+                dt /= 10
             else:
                 probNotOk = False
         actionInd = np.searchsorted(cumProb, r)
@@ -264,51 +288,46 @@ class Simulator:
             self.dotArray.executeStep(actionInd)
         if printState:
             self.printState()
+        return dt
 
     def calcCurrent(self, t,print=False):
-        # if fullOutput:
-        #     n = []
-        #     Q = []
-        #TODO: find smart way to know we have reach steady state
-        steadySteps = 1000
-
-        # Reaching equilibrium before calculating current
-        for steps in steadySteps:
-            self.executeStep(printState=print)
-            self.dotArray.resetCharge()
+        self.dotArray.resetCharge()
         # Now when we are in steady state actually measuring current
-        for steps in range(int(t // self.dt)):
-            self.executeStep(printState=print)
-        current = self.dotArray.getCharge() / t
-        return current
+        curr_t = 0
+        while curr_t < t:
+            curr_t += self.executeStep(t - curr_t, printState=print)
+        rightCurrent, leftCurrent = self.dotArray.getCharge()
+        return rightCurrent/t, -leftCurrent/t
+
+    def checkEquilibration(self, t, rep):
+        Ileft = []
+        Iright = []
+        for r in range(rep):
+            rightCurrent, leftCurrnet = self.calcCurrent(t)
+            Ileft.append(leftCurrnet)
+            Iright.append(rightCurrent)
+        x = t*np.arange(rep)
+        print("Mean right: " + str(np.average(Iright))+ " Var right: " + str(np.var(Iright)))
+        print("Mean left: " + str(np.average(Ileft))+ " Var left: " + str(np.var(Ileft)))
+        plt.plot(x,Ileft, x,Iright)
+        plt.show()
 
     def calcIV(self, Vmax, Vstep, tStep, print=False, fullOutput=False):
         V = []
         I = []
-
         V0 = self.Vext
         while(self.Vext < Vmax):
-            if fullOutput:
-                current, stepn, stepQ = self.calcCurrent(tStep,print=print)
-                I.append(current)
-            else:
-                I.append(self.calcCurrent(tStep, print=print))
+            rightCurrent, leftCurrnet = self.calcCurrent(tStep, print=print)
+            I.append((rightCurrent+leftCurrnet)/2)
             V.append(self.Vext)
             self.Vext += Vstep
-            self.t += tStep
             self.dotArray.changeVext(self.Vext)
-            self.dotArray.resetCharge()
         while(self.Vext > V0):
-            if fullOutput:
-                current, stepn, stepQ = self.calcCurrent(tStep,print=print)
-                I.append(current)
-            else:
-                I.append(self.calcCurrent(tStep, print=print))
+            rightCurrent, leftCurrnet = self.calcCurrent(tStep, print=print)
+            I.append((rightCurrent+leftCurrnet)/2)
             V.append(self.Vext)
             self.Vext -= Vstep
-            self.t += tStep
             self.dotArray.changeVext(self.Vext)
-            self.dotArray.resetCharge()
         return np.array(I), np.array(V)
 
     def printState(self):
@@ -316,15 +335,21 @@ class Simulator:
         self.dotArray.printState()
 
 
-def run1DSimulation(Vext0, VG0, dt, Q0, n0, CG, RG, Ch, Cv, Rh, Rv, rows, columns,
+def run1DSimulation(Vext0, VG0, Q0, n0, CG, RG, Ch, Cv, Rh, Rv, rows, columns,
                     Vmax, Vstep, tStep,repeats=1, savePath=".",
-                    fileName="", fullOutput=False, printState=False):
-
+                    fileName="", fullOutput=False, printState=False, checkEquilibration=False):
+    if checkEquilibration:
+        simulator = Simulator(rows, columns, Vext0, np.array(VG0),
+                              np.array(Q0), np.array(n0), np.array(CG),
+                              np.array(RG), np.array(Ch), np.array(Cv),
+                              np.array(Rh), np.array(Rv))
+        simulator.checkEquilibration(tStep, repeats)
+        return None, None
     basePath = os.path.join(savePath, fileName)
     Is = []
 
     for repeat in range(repeats):
-        simulator = Simulator(rows, columns, Vext0, np.array(VG0), dt,
+        simulator = Simulator(rows, columns, Vext0, np.array(VG0),
                               np.array(Q0), np.array(n0), np.array(CG),
                               np.array(RG), np.array(Ch), np.array(Cv),
                               np.array(Rh), np.array(Rv))
@@ -459,36 +484,76 @@ def create_random_array(M,N, avg, std, only_positive=False):
         return np.random.normal(loc=avg, scale=std, size=(M,N))
 
 if __name__ == "__main__":
-    options, args = getOptions()
-    columns = options.N
-    Vext0 = options.Vmin
-    VG = create_random_array(1, columns, options.VG_avg, options.VG_std,
-                             False)
-    dt = options.dt
-    Q0 = create_random_array(1, columns, options.Q0_avg, options.Q0_std,
-                             False)
-    n0 = create_random_array(1, columns, options.n0_avg, options.n0_std, True)
-    CG = create_random_array(1, columns, options.CG_avg, options.CG_std, True)
-    RG = create_random_array(1, columns, options.RG_avg, options.RG_std, True)
-    C = create_random_array(1, columns + 1, options.C_avg, options.C_std,
-                            True)
-    R = create_random_array(1, columns + 1, options.R_avg, options.R_std,
-                            True)
-    Vmax = options.Vmax
-    Vstep = options.vStep
-    tStep = options.tStep
-    repeats = options.repeats
-    savePath = options.output_folder
-    fileName = options.fileName
-    fullOutput = options.fullOutput
 
+
+    pr = cProfile.Profile()
+    pr.enable()
+    # Initializing Running Parameters
+    # options, args = getOptions()
+    # rows = options.M
+    # columns = options.N
+    # Vext0 = options.Vmin
+    # VG = create_random_array(rows, columns, options.VG_avg, options.VG_std,
+    #                          False)
+    # dt = options.dt
+    # Q0 = create_random_array(rows, columns, options.Q0_avg, options.Q0_std,
+    #                          False)
+    # n0 = create_random_array(rows, columns, options.n0_avg, options.n0_std, True)
+    # CG = create_random_array(rows, columns, options.CG_avg, options.CG_std, True)
+    # RG = create_random_array(rows, columns, options.RG_avg, options.RG_std, True)
+    # Ch = create_random_array(rows, columns + 1, options.C_avg, options.C_std,
+    #                         True)
+    # Cv = create_random_array(rows - 1, columns, options.C_avg, options.C_std,
+    #                         True)
+    # Rh = create_random_array(rows, columns + 1, options.R_avg, options.R_std,
+    #                         True)
+    # Rh = create_random_array(rows - 1, columns, options.R_avg, options.R_std,
+    #                         True)
+    # Vmax = options.Vmax
+    # Vstep = options.vStep
+    # tStep = options.tStep
+    # repeats = options.repeats
+    # savePath = options.output_folder
+    # fileName = options.fileName
+    # fullOutput = options.fullOutput
+
+    # Debug
+    rows = 1
+    columns = 1
+    Vext0 = -0.25
+    VG = [[0.05]]
+    dt = 0.001
+    Q0 = [[0]]
+    n0 = [[0]]
+    CG = [[10]]
+    RG = [[1000]]
+    Ch = [[1,1]]
+    Cv = [[]]
+    Rh = [[1,1]]
+    Rv = [[]]
+    Vmax = 2
+    Vstep = 0.01
+    tStep = 1000
+    repeats = 1
+    savePath = "dbg_2D"
+    fileName = "dbg_2D_try1"
+    fullOutput = False
+
+
+    # Running Simulation
     if not os.path.exists(savePath):
         os.mkdir(savePath)
     elif not os.path.isdir(savePath):
         print("the given path exists but is a file")
         exit(0)
 
-    saveParameters(savePath, fileName, options)
-    run1DSimulation(Vext0, VG, dt, Q0, n0, CG, RG, C, R, columns,
+    # saveParameters(savePath, fileName, options)
+
+    run1DSimulation(Vext0, VG, Q0, n0, CG, RG, Ch, Cv, Rh, Rv, rows,  columns,
                     Vmax, Vstep, tStep, repeats=repeats,
-                    savePath=savePath, fileName=fileName, printState=False)
+                    savePath=savePath, fileName=fileName, printState=False, checkEquilibration=False)
+
+    pr.disable()
+    sortby = 'cumulative'
+    ps = pstats.Stats(pr).sort_stats(sortby)
+    ps.print_stats()
