893439efed1bf4fe8dc0cefaecd192f97d315607
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-09-23 19:55:01 -0400

    fix

--- RC_SET/run_2d_array_on_slurm.sh
+++ RC_SET/run_2d_array_on_slurm.sh
@@ -14,20 +14,20 @@ q_std=0
 vstep=0.1
 f="2D_Array/first_run"
 
-sbatch --export=M=1,N=1,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=0,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="no_disorder_1_1",out="$f",time="01:00:00" run_RC_SET_on_slurm.sh
+sbatch --export=M=1,N=1,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=0,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="no_disorder_1_1",out="$f",time="1:00:00" run_RC_SET_on_slurm.sh
 
 sleep 3
 
-sbatch --export=M=1,N=1,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=1,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="r_disorder_1_1",out="$f",time="01:00:00" run_RC_SET_on_slurm.sh  
+sbatch --export=M=1,N=1,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=1,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="r_disorder_1_1",out="$f",time="1:00:00" run_RC_SET_on_slurm.sh  
 
 sleep 3
 
-sbatch --export=M=1,N=10,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=0,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="no_disorder_1_10",out="$f",time="05:00:00" run_RC_SET_on_slurm.sh  
+sbatch --export=M=1,N=10,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=0,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="no_disorder_1_10",out="$f",time="5:00:00" run_RC_SET_on_slurm.sh  
 
 sleep 3
 
 
-sbatch --export=M=1,N=10,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=1,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="r_disorder_1_10",out="$f",time="05:00:00" run_RC_SET_on_slurm.sh  
+sbatch --export=M=1,N=10,vmin="$vmin",vmax=2,vstep="$vstep",vg_avg="$vg_avg",vg_std=0,c_avg="$c_avg",cstd=0,cg_avg="$cg_avg",cg_std=0,r_avg="$r_avg",r_std=1,rg_avg="$rg_avg",rg_std=0,repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="r_disorder_1_10",out="$f",time="5:00:00" run_RC_SET_on_slurm.sh  
 
 sleep 3
 
