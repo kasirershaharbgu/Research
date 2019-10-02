577b3f41feda34c91e2ab9e5ca4a57e828fcdd9c
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-10-02 14:21:10 -0400

    fix slurm scrupts

--- RC_SET/run_RC_SET_on_slurm.sh
+++ RC_SET/run_RC_SET_on_slurm.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-#SBATCH -p dept                            # partition/queue name
-#SBATCH --mem=2000MB                       # memory in MB
-#SBATCH --mail-user=skasirer@princeton.edu # Mail  id of the user
-#SBATCH --mail-type=end                    # Slurm will send at the completion of your job
-
 . /etc/local/set-anaconda3
 python3 dots_array.py -M "$M" -N "$N" --vmin "$vmin" --vmax "$vmax" --vstep "$vstep" --vr "$vr" --vg-avg "$vg_avg" --vg-std "$v_std" --c-avg "$c_avg" --c-std "$c_std" --cg-avg "$cg_avg" --cg-std "$cg_std" --r-avg "$r_avg" --r-std "$r_std" --rg-avg "$rg_avg" --rg-std "$rg_std" --repeats "$repeats" --n-avg "$n_avg" --n-std "$n_std" --q-avg "$q_avg" --q-std "$q_std" --distribution "$dist" --file-name "$file_name" -o "$out"
