c3ab49fad31090a6af68d80299c826efe00f5ebb
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-10-04 18:05:51 -0400

    big array script

--- RC_SET/run_small_2d_array_on_slurm.sh
+++ RC_SET/run_small_2d_array_on_slurm.sh
@@ -17,44 +17,77 @@ q_std=0
 vstep=0.01
 rg_avg=1000
 cg_avg=10
-f="big_2D_square_arrays"
+t="0:30:00"
+f="small_2D_arrays"
 
-t="1-10:00:00"
-rows=5
-columns=5
-vmax=10
-
+rows=2
+columns=1
+vmax=4
 dist="two_points"
-name="no_disorder_5_5"
+name="no_disorder_2_1"
 r_std=0
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
 
-dist="uniform"
-name="uniform_disorder_5_5"
-r_std=9
-sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
-
+rows=1
+columns=2
+vmax=4
 dist="two_points"
-name="two_points_disorder_5_5"
-r_std=9
-sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
-
-t="6-10:00:00"
-rows=10
-columns=10
-vmax=20
-
-dist="two_points"
-name="no_disorder_10_10"
+name="no_disorder_1_2"
 r_std=0
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
 
-dist="uniform"
-name="uniform_disorder_10_10"
+rows=2
+columns=2
+vmax=4
+dist="two_points"
+name="no_disorder_2_2"
+r_std=0
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
+rows=2
+columns=1
+vmax=4
+dist="two_points"
+name="two_points_disorder_2_1"
 r_std=9
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
 
+rows=1
+columns=2
+vmax=4
 dist="two_points"
-name="two_points_disorder_10_10"
+name="two_points_disorder_1_2"
+r_std=9
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
+rows=2
+columns=2
+vmax=4
+dist="two_points"
+name="two_points_disorder_2_2"
+r_std=9
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
+rows=2
+columns=1
+vmax=4
+dist="uniform"
+name="uniform_disorder_2_1"
+r_std=9
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
+rows=1
+columns=2
+vmax=4
+dist="uniform"
+name="uniform_disorder_1_2"
+r_std=9
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
+rows=2
+columns=2
+vmax=4
+dist="uniform"
+name="uniform_disorder_2_2"
 r_std=9
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
