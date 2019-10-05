0729c1b8c2ab1c6c81b1568c24099ddcee3b9cb0
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-10-04 23:58:54 -0400

    again 1dot

--- RC_SET/run_single_dot_array_on_slurm.sh
+++ RC_SET/run_single_dot_array_on_slurm.sh
@@ -19,15 +19,17 @@ vmax=2
 dist="two_points"
 rows=1
 columns=1
-t="0:30:00"
+t="00:30:00"
 
-f="single_dot_fast_relaxation"
+f="Single_dot_fast_relaxation"
 rg_avg=0.001
 
 cg_avg=10
 name="no_disorder_big_cg"
 r_std=0
+
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
+
 
 cg_avg=1
 name="no_disorder_small_cg"
@@ -44,7 +46,7 @@ name="r_disorder_small_cg"
 r_std=9
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist" run_RC_SET_on_slurm.sh
 
-f="single_dot_slow_relaxation"
+f="Single_dot_slow_relaxation"
 rg_avg=1000
 
 cg_avg=10
