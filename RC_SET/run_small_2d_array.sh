eae9ee1bdf367aa5611c4b10640eb4b3c0828a4a
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-10-24 14:35:25 -0400

    still on scripts

--- RC_SET/run_small_2d_array.sh
+++ RC_SET/run_small_2d_array.sh
@@ -18,36 +18,36 @@ rg_avg=1000
 cg_avg=10
 dist="two_points"
 r_std=0
-graph="\"\""
+graph=""
 full="--full"
-currentmap="\"\""
+currentmap=""
 vmax=3
-out="2_dots_arrays_gillespie"
-if [ ! -d "$out" ]; then
-  mkdir "$out"
+f="2_dots_arrays_gillespie"
+if [ ! -d "$f" ]; then
+  mkdir "$f"
 fi
 
-N=1
-M=2
+rows=1
+columns=2
 vg_avg=1
-file_name="increasing_r_vg_1"
+name="increasing_r_vg_1"
 custom_rh="\"[[1,10,20]]\""
 custom_rv="\"[[]]\""
-qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
+qsub -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",graph="$graph" run_RC_SET.sh
 
-file_name="decreasing_r_vg_1"
+name="decreasing_r_vg_1"
 custom_rh="\"[[20,10,1]]\""
 custom_rv="\"[[]]\""
-qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
+qsub -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",graph="$graph" run_RC_SET.sh
 
 vg_avg=1
 vg_std=1
-file_name="increasing_r_noisy_vg"
+name="increasing_r_noisy_vg"
 custom_rh="\"[[1,10,20]]\""
 custom_rv="\"[[]]\""
-qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
+qsub -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",graph="$graph" run_RC_SET.sh
 
-file_name="decreasing_r_noisy_vg"
+name="decreasing_r_noisy_vg"
 custom_rh="\"[[20,10,1]]\""
 custom_rv="\"[[]]\""
-qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
+qsub -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",graph="$graph" run_RC_SET.sh
