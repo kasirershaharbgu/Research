3d69e328e50497004f00d4eea45a651e9106dbdd
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-10-24 14:28:16 -0400

    adding config to bash

--- RC_SET/run_2d_array.sh
+++ RC_SET/run_2d_array.sh
@@ -32,22 +32,22 @@ fi
 dist="two_points"
 file_name="no_disorder_5_5"
 r_std=0
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
 dist="uniform"
 name="uniform_disorder_5_5"
 r_std=9
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
 dist="two_points"
 file_name="two_points_disorder_5_5"
 r_std=9
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
 dist="exp"
 file_name="exp_disorder_5_5"
 r_std=9
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
 N=2
 M=2
@@ -55,11 +55,11 @@ custom_rh="\"[[10,10,10],[1,1,1]]\""
 custom_rv="\"[[10,10]]\""
 dist="uniform"
 file_name="one_nig_one_small_2_2"
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
 custom_rh="\"[[1,10,10],[10,1,1]]\""
 custom_rv="\"[[1,10]]\""
 dist="uniform"
 file_name="zigzag_2_2"
-qsub -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" -V run_RC_SET.sh
+qsub -V -S /bin/bash -cwd -N "$file_name" -q jdubi.q -o "$out/$file_name.out" run_RC_SET.sh
 
