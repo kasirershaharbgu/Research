aa2998f080386f88382a29be6a87415a76cc52c9
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-11-04 16:27:01 -0500

    small arrays lower v scripts for slurm

--- RC_SET/run_small_2d_array_on_slurm.sh
+++ RC_SET/run_small_2d_array_on_slurm.sh
@@ -14,18 +14,17 @@ n_avg=0
 n_std=0
 q_avg=0
 q_std=0
-vstep=0.1
+vstep=0.01
 rg_avg=1000
 cg_avg=10
 t="20:00:00"
 dist="uniform"
 r_std=0
 graph=""
-full="--full"
 currentmap="--current-map"
-resume="--resume"
-vmax=30
-f="2_vertical_dots_arrays_c_disorder"
+resume=""
+vmax=2
+f="2_vertical_dots_arrays_c_disorder_small_v"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -37,26 +36,40 @@ name="const_r_increasing_c_2_1"
 custom_rh="\"[[1,1],[1,1]]\""
 custom_rv="\"[[1]]\""
 custom_ch="\"[[1,10],[1,10]]\""
-custom_cv="\"[[1]]\""
+custom_cv="\"[[10]]\""
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
 
 name="decreasing_r_decreasing_c_2_1"
 custom_rh="\"[[10,1],[10,1]]\""
 custom_rv="\"[[1]]\""
 custom_ch="\"[[10,1],[10,1]]\""
-custom_cv="\"[[1]]\""
+custom_cv="\"[[10]]\""
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
 
 name="increasing_r_zigzag_c_2_1"
 custom_rh="\"[[1,10],[1,10]]\""
 custom_rv="\"[[1]]\""
 custom_ch="\"[[10,1],[1,10]]\""
-custom_cv="\"[[1]]\""
+custom_cv="\"[[10]]\""
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
 
 name="zigzag_r_zigzag_c_2_1"
 custom_rh="\"[[10,1],[1,10]]\""
 custom_rv="\"[[1]]\""
 custom_ch="\"[[10,1],[1,10]]\""
-custom_cv="\"[[1]]\""
+custom_cv="\"[[10]]\""
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
+
+name="decreasing_r_increasing_c_2_1"
+custom_rh="\"[[10,1],[10,1]]\""
+custom_rv="\"[[1]]\""
+custom_ch="\"[[1,10],[1,10]]\""
+custom_cv="\"[[10]]\""
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
+
+name="increasing_r_const_c_2_1"
+custom_rh="\"[[1,10],[1,10]]\""
+custom_rv="\"[[1]]\""
+custom_ch="\"[[1,1],[1,1]]\""
+custom_cv="\"[[10]]\""
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=2000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_rh",custom_cv="$custom_rv",graph="$graph",resume="$resume" run_RC_SET_on_slurm.sh
