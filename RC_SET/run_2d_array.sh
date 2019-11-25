258b4ce98b17ec81ed517f52c3639a0b39135ba6
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2019-11-25 14:31:15 -0500

    new script for bgu server

--- RC_SET/run_2d_array.sh
+++ RC_SET/run_2d_array.sh
@@ -3,22 +3,28 @@
 vmin=0
 vr=0
 vg_avg=0
+vg_std=0
 c_avg=1
+c_std=0
 r_avg=10
+r_std=0
 rg_std=0
 repeats=20
 n_avg=0
 n_std=0
 q_avg=0
 q_std=0
-vstep=1
+vstep=0.01
 rg_avg=1000
 cg_avg=10
 f="big_array"
 full="" 
 graph=""
+resume=""
 currentmap="--current-map"
-vmax=100
+vmax=2
+vSym="--symmetric-v"
+T=0.01
 custom_rh="\"\""
 custom_rv="\"\""
 dist="exp"
@@ -27,12 +33,11 @@ if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
 
-rows=20
-columns=20
+rows=10
+columns=10
 
 c_std=0.5
-cg_std=1
-r_std=5
+r_std=9
 vg_std=0
-name="array_20_20"
-qsub -V -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" run_RC_SET.sh
+name="array_10_10"
+qsub -V -S /bin/bash -cwd -N "$name" -q jdubi.q -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="T",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -vcustom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
