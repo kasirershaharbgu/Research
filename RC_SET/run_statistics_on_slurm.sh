936c7f80d7308bae48f39da075ea0d5f1bbf6a3a
Shahar Kasirer <skasirer@princeton.edu>
2019-12-10 16:41:37 -0500

    slurm results

--- RC_SET/run_statistics_on_slurm.sh
+++ RC_SET/run_statistics_on_slurm.sh
@@ -22,7 +22,7 @@ dist="exp"
 r_std=0
 graph=""
 currentmap=""
-resume=""
+resume="--resume"
 vSym="--symmetric-v"
 resume=""
 custom_rh="\"\""
@@ -38,9 +38,9 @@ fi
 rows=3
 columns=3
 c_std=0.4
-for r_std in 1 2 3  
+for r_std in 9
 do
-	for run in 1 2 3 4 5 6 7 8 9 10
+	for run in 1
 	do
 	name="c_std_${c_std}_r_std_${r_std}_run_${run}"
 	sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=5000MB --time="$t" --partition=dept --mail-user=skasirer@princeton.edu --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_ch",custom_cv="$custom_cv",graph="$graph",resume="$resume",vSym="$vSym" run_RC_SET_on_slurm.sh
