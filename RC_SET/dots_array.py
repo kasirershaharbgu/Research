8ff675b448555e372c432ac3a5e1fbcaea0e3eca
shahar kasirer <shahar.kasirer@mail.huji.ac.il>
2020-03-04 11:47:03 +0200

    fixing bug in loading from checkpoint

--- RC_SET/dots_array.py
+++ RC_SET/dots_array.py
@@ -1147,13 +1147,13 @@ class Simulator:
         if fullOutput:
             ns = np.load(baseName + "_ns.npy")
             if len(ns) > loadLen:
-                ns = ns[:, loadLen, :, :]
+                ns = ns[loadLen, :, :]
             Qs = np.load(baseName + "_Qs.npy")
             if len(Qs) > loadLen:
-                Qs = Qs[:, loadLen, :, :]
+                Qs = Qs[loadLen, :, :]
             nsErr = np.load(baseName + "_nsErr.npy")
             if len(nsErr) > loadLen:
-                nsErr = nsErr[:, loadLen, :, :]
+                nsErr = nsErr[loadLen, :, :]
             QsErr = np.load(baseName + "_QsErr.npy")
             if len(QsErr) > loadLen:
                 QsErr = QsErr[:, loadLen, :, :]
