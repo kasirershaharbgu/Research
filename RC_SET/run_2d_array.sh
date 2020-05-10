dd5565729c7fca905e378e16c1e88a86cebd6867
kasirer <kasirer@post.bgu.ac.il>
2020-05-10 10:36:20 +0300

    updating script for bgu

--- RC_SET/run_2d_array.sh
+++ RC_SET/run_2d_array.sh
@@ -47,6 +47,17 @@ c_std=9.5
 r_avg=10
 r_std=9
 
+rows=10
+columns=10
+for run in 1 2 3 4 5
+do
+	name="array_${rows}_${columns}_c_r_disorder_run_${run}"
+	qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",file_name="$name",out="$f",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
+sleep 1
+done
+
+rows=5
+columns=5
 for run in 1 2 3 4 5
 do
 	name="array_${rows}_${columns}_c_r_disorder_run_${run}"
