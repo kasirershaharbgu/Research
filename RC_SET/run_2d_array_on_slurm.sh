fb155eb2a90a45e6538b56a04fc397eaffb63679
Shahar Kasirer <skasirer@princeton.edu>
2020-05-16 09:03:03 -0400

    slurm resuts

--- RC_SET/run_2d_array_on_slurm.sh
+++ RC_SET/run_2d_array_on_slurm.sh
@@ -15,6 +15,7 @@ q_std=0
 vstep=0.01
 rg_avg=500
 cg_avg=10
+t="5-12:00:00"
 dist="exp"
 graph=""
 currentmap="--current-map"
@@ -31,7 +32,7 @@ custom_cv="\"\""
 T=0
 vmax=4
 efermi=""
-f="2d_array_bgu_custom_paths"
+f="2d_array_slurm_custom_paths"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -48,10 +49,8 @@ custom_rv="\"[[1,1,1,1,1,1,1,1,1,10],[1,1,1,1,1,1,1,1,1,10],[1,1,1,1,1,1,1,1,1,1
 custom_ch="\"\""
 custom_cv="\"\""
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=10GB --time="$t" --partition=dept --mail-user=kasirer@post.bgu.ac.il --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",file_name="$name",out="$f",inpu"$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_ch",custom_cv="$custom_cv",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi" run_RC_SET_on_slurm.sh
-sleep 1
 
-
-f="2d_array_bgu_big_c_disorder"
+f="2d_array_slurm_big_c_disorder"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
