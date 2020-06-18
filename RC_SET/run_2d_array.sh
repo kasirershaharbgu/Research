5f15d1a4ef3ae076b81842c753291bd7ea2b92b2
kasirer <kasirer@post.bgu.ac.il>
2020-06-18 19:01:25 +0300

    run many simulations fo satistics script

--- RC_SET/run_2d_array.sh
+++ RC_SET/run_2d_array.sh
@@ -7,7 +7,7 @@ vg_avg=0
 vg_std=0
 c_avg=1
 r_avg=1
-repeats=20
+repeats=50
 n_avg=0
 n_std=0
 q_avg=0
@@ -39,7 +39,7 @@ vmax=1.1
 efermi=""
 
 
-f="bgu_2d_finite_temperature_different_disorders"
+f="bgu_2d_finite_temperature_different_disorders_statistics"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -48,18 +48,16 @@ custom_rv="\"\""
 ustom_ch="\"\""
 custom_cv="\"\""
 c_avg=2
-c_std=1
+c_std=0.1
 r_avg=10
 r_std=9
 rows=10
 columns=10
 double_loop=""
 double_time=""
-for c_std in 0 0.1 0.5 1
-do
-	for r_std in 1 5 9
+	for r_std in 5 5.5 6 6.5 7 7.5 8 8.5 9 9.5
 	do
-		for run in 1 2 3 4 5 
+		for run in 1 2 3 4 5 6 7 8 9 10
 		do
 			name="array_10_10_disorder_c_std_${c_std}_r_std_${r_std}_run_${run}"
 			qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
