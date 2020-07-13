b73f79eb52a3e333d95d140284c8a1a7a3a4dc98
kasirer <kasirer@post.bgu.ac.il>
2020-07-13 11:24:56 +0300

    script to check different disorder affect on jumps

--- RC_SET/run_2d_array.sh
+++ RC_SET/run_2d_array.sh
@@ -28,16 +28,15 @@ efermi=""
 dbg=""
 double_time=""
 double_loop=""
-input="same_array_different_temperature/runningParameters_array_10_10_disorder_c_std_0.1_r_std_9_run_1.txt"
 custom_rh="\"\""
 custom_rv="\"\""
 custom_ch="\"\""
 custom_cv="\"\""
 temperature_gradient=0
-vmax=1.2
+vmax=0.7
 efermi=""
 gap=0
-
+T=0.001
 f="same_array_different_temperature"
 if [ ! -d "$f" ]; then
   mkdir "$f"
@@ -50,13 +49,15 @@ c_avg=2
 c_std=0.1
 r_avg=10
 r_std=9
-rows=10
-columns=10
+rows=15
+columns=5
 double_loop=""
 double_time=""
-for T in 0.001 0.002 0.005 0.007 0.01 0.015 0.02 0.03 0.05 0.08 0.1
-do
-	name="array_10_10_T_$T"
-	qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
-		sleep 1
-done	
+input="bgu_2d_narrow_arrays/runningParameters_array_5_15_disorder_c_std_0_r_std_9_run_10.txt" 
+name="array_5_15_disorder_c_std_0_r_std_9_run_10"
+qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
+sleep 1
+input="bgu_2d_narrow_arrays/runningParameters_array_5_15_disorder_c_std_0.1_r_std_9_run_10_high_r_disorder.txt" 
+name="array_5_15_disorder_c_std_0.1_r_std_9_run_10_high_r_disorder"
+qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
+sleep 1	
