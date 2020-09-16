d28190ce28f1349678466411cf871575adf3041b
kasirer <kasirer@post.bgu.ac.il>
2020-09-16 09:00:38 +0300

    new script for small horizontal arrays

--- RC_SET/running_scripts/run_small_2d_array.sh
+++ RC_SET/running_scripts/run_small_2d_array.sh
@@ -36,7 +36,7 @@ vmax=2
 efermi=""
 gap=0
 T=0
-f="bgu_1d_vertical_arrays"
+f="bgu_1d_horizontal_arrays"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -44,28 +44,28 @@ c_avg=1
 c_std=0.9
 r_avg=10
 r_std=9.5
-columns=1
+
 double_loop=""
 double_time=""
 input=""
 
+rows=1
+columns=2
 for cg_avg in 1 5 10 50
 do
-rows=2
-name="array_${rows}_1_custome_r_c_disorder_cg_${cg_avg}_run_1"
-custom_rh="\"[[1,19],[1,19]]\""
-custom_rv="\"[[10]]\""
-custom_ch="\"[[0.1,0.1],[1.9,1.9]]\""
-custom_cv="\"[[1]]\""
+name="array_${rows}_${columns}_custome_r_c_disorder_cg_${cg_avg}_run_0"
+custom_rh="\"[[1,10,19]]\""
+custom_rv="\"[[]]\""
+custom_ch="\"[[0.1,1.9,1]]\""
+custom_cv="\"[[]]\""
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
 
-rows=3
-name="array_${rows}_1_custome_r_c_disorder_cg_${cg_avg}_run_1"
-custom_rh="\"[[1,19],[1,19],[1,19]]\""
-custom_rv="\"[[10],[10]]\""
-custom_ch="\"[[0.1,0.1],[1,1],[1.9,1.9]]\""
-custom_cv="\"[[1],[1]]\""
+name="array_${rows}_${columns}_custome_r_c_disorder_cg_${cg_avg}_run_1"
+custom_rh="\"[[1,10,19]]\""
+custom_rv="\"[[]]\""
+custom_ch="\"[[1,0.1,1.9]]\""
+custom_cv="\"[[]]\""
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
 done
