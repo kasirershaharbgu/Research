119b4a7238101267943b17ba4d1411c0a96006b3
kasirershaharbgu <kasirer@post.bgu.ac.il>
2020-10-17 10:35:43 +0300

    bgu results

--- RC_SET/running_scripts/run_single_dot_array.sh
+++ RC_SET/running_scripts/run_single_dot_array.sh
@@ -3,6 +3,7 @@ flags="-pe shared 14 -V -S /bin/bash -q jdubi.q -cwd"
 rows=1
 columns=1
 vr=0
+vmin=0
 vg_avg=0
 vg_std=0
 c_avg=1
@@ -104,7 +105,7 @@ custom_ch="\"[[0.6,0.6]]\""
 custom_cv="\"[[]]\""
 custom_rh="\"[[1,19]]\""
 custom_rv="\"[[]]\""
-name="array_3_1_cg_10_row_0"
+name="array_3_1_cg_50_row_0"
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",calc_it=$calc_it,dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
 
@@ -112,7 +113,7 @@ custom_ch="\"[[2,2]]\""
 custom_cv="\"[[]]\""
 custom_rh="\"[[1,19]]\""
 custom_rv="\"[[]]\""
-name="array_3_1_cg_10_row_1"
+name="array_3_1_cg_50_row_1"
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",calc_it=$calc_it,dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
 
@@ -120,6 +121,6 @@ custom_ch="\"[[2.4,2.4]]\""
 custom_cv="\"[[]]\""
 custom_rh="\"[[1,19]]\""
 custom_rv="\"[[]]\""
-name="array_3_1_cg_10_row_2"
+name="array_3_1_cg_50_row_2"
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",calc_it=$calc_it,dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
