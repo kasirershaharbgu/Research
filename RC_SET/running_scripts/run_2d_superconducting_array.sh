85e95128fe59f18dff683bb015626f33caf40d08
kasirer <kasirer@post.bgu.ac.il>
2020-11-30 10:13:49 +0200

    Superconducting arrays results.

--- RC_SET/running_scripts/run_2d_superconducting_array.sh
+++ RC_SET/running_scripts/run_2d_superconducting_array.sh
@@ -6,14 +6,16 @@ vr=0
 vg_avg=0
 vg_std=0
 c_avg=1
-r_avg=1
-repeats=20
+c_std=0.9
+r_avg=10
+r_std=9
+repeats=50
 n_avg=0
 n_std=0
 q_avg=0
 q_std=0
-vstep=1
-rg_avg=500
+vstep=0.001
+rg_avg=1000
 rg_std=0
 cg_avg=10
 cg_std=0
@@ -28,36 +30,28 @@ efermi=""
 dbg=""
 double_time=""
 double_loop=""
-input=""
+input="super_conducting_array_different_temperatures/runningParameters_array_10_10_disorder_c_std_0.1_r_std_9_run_1.txt"
 custom_rh="\"\""
 custom_rv="\"\""
 custom_ch="\"\""
 custom_cv="\"\""
 temperature_gradient=0
-T=0.001
-vmax=10
+vmax=1.5
 efermi=""
 superconducting="--superconducting"
-gap=0.2
-
-f="bgu_2d_superconducting_array"
+gap=0.1
+f="super_conducting_array_different_temperatures"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
-custom_rh="\"\""
-custom_rv="\"\""
-ustom_ch="\"\""
-custom_cv="\"\""
-c_avg=2
-c_std=1
-r_avg=10
-r_std=9
 rows=10
 columns=10
-double_loop=""
-double_time=""
-c_std=1
-r_std=9
-name="array_10_10_disorder_c_std_${c_std}_r_std_${r_std}"
-qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",superconducting="$superconducting",gap=$gap,leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
-sleep 1
+for T in 0.001 0.005
+do
+	for cg_avg in 1 5
+	do
+	name="sc_array_${rows}_${columns}_T_${T}"
+	qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",superconducting="$superconducting",gap=$gap,leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" run_RC_SET.sh
+	sleep 1
+	done
+done
