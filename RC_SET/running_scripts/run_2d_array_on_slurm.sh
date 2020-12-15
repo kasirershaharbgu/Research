e87b678c894fecc3a004986946f2fb614b79d313
kasirer <kasirer@post.bgu.ac.il>
2020-12-15 09:39:09 +0200

    script for sc arrays with different gap

--- RC_SET/running_scripts/run_2d_array_on_slurm.sh
+++ RC_SET/running_scripts/run_2d_array_on_slurm.sh
@@ -30,15 +30,13 @@ dbg=""
 double_time=""
 double_loop=""
 superconducting="--superconducting"
-gap=0.1
 custom_rh="\"\""
 custom_rv="\"\""
 custom_ch="\"\""
 custom_cv="\"\""
 T=0.001
-vmax=2.5
 efermi=""
-f="super_conducting_array_different_temperatures"
+f="super_conducting_array_different_temperatures/"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -52,9 +50,33 @@ r_std=20
 rows=5
 columns=5
 
-for run in 1 2 3
+
+run=1
+input="super_conducting_array_different_temperatures/runningParameters_sc_array_5_5_T_0.001_cg_10_run_1.txt"
+vmax=4
+for gap in 0.001 1
 do
-name="sc_array_${rows}_${columns}_T_${T}_cg_${cg_avg}_run_${run}"
+name="sc_array_${rows}_${columns}_T_${T}_cg_${cg_avg}_run_${run}_gap_${gap}"
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=10GB --time="$t" --partition=dept --mail-user=kasirer@post.bgu.ac.il --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_ch",custom_cv="$custom_cv",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",superconducting="$superconducting",gap="$gap",input="$input",dbg="$dbg" running_scripts/run_RC_SET_on_slurm.sh
+sleep 1
+done
+
+run=2
+input="super_conducting_array_different_temperatures/runningParameters_sc_array_5_5_T_0.001_cg_10_run_2.txt"
+vmax=5
+for gap in 0.001 0.1 1
+do
+name="sc_array_${rows}_${columns}_T_${T}_cg_${cg_avg}_run_${run}_gap_${gap}"
+sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=10GB --time="$t" --partition=dept --mail-user=kasirer@post.bgu.ac.il --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_ch",custom_cv="$custom_cv",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",superconducting="$superconducting",gap="$gap",input="$input",dbg="$dbg" running_scripts/run_RC_SET_on_slurm.sh
+sleep 1
+done
+
+run=3
+input="super_conducting_array_different_temperatures/runningParameters_sc_array_5_5_T_0.001_cg_10_run_3.txt"
+vmax=5.5
+for gap in 0.001 0.1 1
+do
+name="sc_array_${rows}_${columns}_T_${T}_cg_${cg_avg}_run_${run}_gap_${gap}"
 sbatch -J="$name" --nodes=1 --ntasks-per-node="$repeats" --mem=10GB --time="$t" --partition=dept --mail-user=kasirer@post.bgu.ac.il --mail-type=end --output="$f/$name.out"  --export=M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",custom_rh="$custom_rh",custom_rv="$custom_rv",custom_ch="$custom_ch",custom_cv="$custom_cv",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",superconducting="$superconducting",gap="$gap",input="$input",dbg="$dbg" running_scripts/run_RC_SET_on_slurm.sh
 sleep 1
 done
