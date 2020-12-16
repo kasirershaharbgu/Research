5dd2a908a3a8ed83926b1906de3b9840e47479a6
kasirer <kasirer@post.bgu.ac.il>
2020-12-16 10:13:49 +0200

    Updating process synchronization schema

--- RC_SET/dots_array.py
+++ RC_SET/dots_array.py
@@ -164,35 +164,41 @@ class TunnelingRateCalculator:
     def isWriting(self):
         if os.path.exists(os.path.join(self.dirName, "writing.txt")):
             writing_process_still_alive = False
-            with open(os.path.join(self.dirName, "writing.txt"), "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    pid = int(line)
-                    try:
-                        os.kill(pid,0)
-                    except OSError:
-                        continue
-                    else:
-                        writing_process_still_alive = True
-            return writing_process_still_alive
+            try:
+                with open(os.path.join(self.dirName, "writing.txt"), "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        pid = int(line)
+                        try:
+                            os.kill(pid,0)
+                        except OSError:
+                            continue
+                        else:
+                            writing_process_still_alive = True
+                return writing_process_still_alive
+            except FileNotFoundError:
+                return False
         else:
             return False
 
     def getWritingLock(self):
-        while self.isWriting():
-            sleep(60)
-        got_it=False
+        got_it = False
         while not got_it:
-            with open(os.path.join(self.dirName, "writing.txt"), "w") as f:
+            while self.isWriting():
+                sleep(60)
+            with open(os.path.join(self.dirName, "writing.txt"), "a") as f:
                 f.write(str(os.getpid())+"\n")
             got_it=True
-            with open(os.path.join(self.dirName, "writing.txt"), "r") as f:
-                lines = f.readlines()
-                for line in lines:
-                    pid = int(line)
-                    if pid < os.getpid():
-                        got_it = False
-                        sleep(60)
+            try:
+                with open(os.path.join(self.dirName, "writing.txt"), "r") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        pid = int(line)
+                        if pid < os.getpid():
+                            got_it = False
+                            sleep(60)
+            except FileNotFoundError:
+                got_it = False
 
     def freeWritingLock(self):
         if self.isWriting():
