6deb5e86747b4531dd7420854910c22d9a980407
kasirer <kasirer@post.bgu.ac.il>
2021-11-01 18:06:14 +0200

    new r disorder simulation with higher RG

--- RC_SET/running_scripts/run_2d_array_increasing_disorder_by_multiplication.sh
+++ RC_SET/running_scripts/run_2d_array_increasing_disorder_by_multiplication.sh
@@ -13,7 +13,7 @@ n_std=0
 q_avg=0
 q_std=0
 vstep=0.001
-rg_avg=500
+rg_avg=5000
 rg_std=0
 cg_std=0
 cg_avg=10
@@ -32,7 +32,7 @@ efermi=""
 input=""
 gap=0
 T=0.001
-f="increase_disorder_by_multiplication"
+f="increase_disorder_by_multiplication_correction"
 if [ ! -d "$f" ]; then
   mkdir "$f"
 fi
@@ -47,12 +47,10 @@ double_time=""
 rows=10
 columns=10
 
-for run in 1 2 3 4 5 6 7 8 9 10
-do
-input="increase_disorder_by_multiplication/runningParameters_array_10_10_disorder_c_std_0.1_r_std_9_run_${run}.txt"
-name="array_10_10_disorder_c_std_0.1_r_std_9.5_run_${run}"
+
+input="increase_disorder_by_multiplication_correction/runningParameters_array_10_10_disorder_c_std_0.1_r_std_9.5_run_1.txt"
+name="array_10_10_disorder_c_std_0.1_r_std_9.5_run_1_high_rg"
 qsub "$flags" -N "$name" -o "$f/$name.out" -v M="$rows",N="$columns",vmin="$vmin",vmax="$vmax",vstep="$vstep",vg_avg="$vg_avg",vg_std="$vg_std",c_avg="$c_avg",c_std="$c_std",cg_avg="$cg_avg",cg_std="$cg_std",r_avg="$r_avg",r_std="$r_std",rg_avg="$rg_avg",rg_std="$rg_std",repeats="$repeats",n_avg="$n_avg",n_std="$n_std",q_avg="$q_avg",q_std="$q_std",T="$T",temperature_gradient="$temperature_gradient",gap="$gap",file_name="$name",out="$f",input="$input",vr="$vr",dist="$dist",full="$full",currentmap="$currentmap",graph="$graph",resume="$resume",vSym="$vSym",leaping="$leaping",efermi="$efermi",double_time="$double_time",double_loop="$double_loop",dbg="$dbg" -v custom_rh="$custom_rh" -v custom_rv="$custom_rv" -v custom_ch="$custom_ch" -v custom_cv="$custom_cv" running_scripts/run_RC_SET.sh
 sleep 1
-done
 
 
